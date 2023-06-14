0285087a9cadc92884cc106fd822cbebb5b41b19
IinoCI04 <117345388+TasukuIino@users.noreply.github.com>
2023-06-14 19:29:08 +0900

    初期データをプッシュ

---
+++ Project_SIGMA/Source/Project_SIGMA/Project_SIGMAGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_SIGMAGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SIGMA_API AProject_SIGMAGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
